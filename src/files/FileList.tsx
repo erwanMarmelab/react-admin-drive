ace5108044cc3703a989b35a1971fe0a5a4597a0
erwanMarmelab <erwan@marmelab.com>
2024-10-28 13:18:23 +0100

    folder navigation ok

--- src/files/FileList.tsx
+++ src/files/FileList.tsx
@@ -2,16 +2,53 @@ import {
   InfiniteList,
   Datagrid,
   EditButton,
-  type InfiniteListProps,
   TextField,
+  type Identifier,
+  type RaRecord,
+  useListContext,
+  Button,
+  useGetOne,
 } from "react-admin";
 import { Icon } from "./Icon";
 
-export const FileList = (props: InfiniteListProps) => (
-  <InfiniteList {...props} sort={{ field: "type", order: "DESC" }}>
+export const FileList = () => (
+  <InfiniteList
+    sort={{ field: "type", order: "ASC" }}
+    filterDefaultValues={{ parentId: 0 }}
+  >
+    <BackButton />
+    <FileDatagrid />
+  </InfiniteList>
+);
+
+const BackButton = () => {
+  const { filterValues, setFilters } = useListContext();
+  const { data, isPending, error } = useGetOne("folders", {
+    id: filterValues.parentId,
+  });
+  if (isPending || error || !data || data.parentId == null) return null;
+  return (
+    <Button
+      onClick={() => {
+        setFilters({ parentId: JSON.stringify(data.parentId) || 0 });
+      }}
+      label={data.name}
+    />
+  );
+};
+
+const FileDatagrid = () => {
+  const { setFilters } = useListContext();
+  return (
     <Datagrid
       sx={{
         "& td:nth-child(2)": { width: "40px", paddingRight: "0" },
+      }}
+      rowClick={(_id: Identifier, _resource: string, record: RaRecord) => {
+        if (record.type === "Folder") {
+          setFilters({ parentId: record.id });
+          return "";
+        } else return "show";
       }}
     >
       <Icon />
@@ -19,5 +56,5 @@ export const FileList = (props: InfiniteListProps) => (
       <TextField source="type" />
       <EditButton />
     </Datagrid>
-  </InfiniteList>
-);
+  );
+};
